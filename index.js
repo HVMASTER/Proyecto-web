19a8b5f7645ebb362a640194202d6b81bc6915a4
Manuel <man.conchar@duocuc.cl>
2023-05-06 22:47:12 -0400

    Se cambia codigo js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-alert("hola mundo")
+const btnCart =document.querySelector('.container-icon')
+        const containerCartProducts = document.querySelector('.container-cart-products')
+  btnCart.addEventListener('click',()=> {
+          containerCartProducts.classList.toggle('hidden-cart')    
+  })
